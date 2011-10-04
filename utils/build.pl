5fedb65371d13ad8f309701420658b8a29dcd734
Chia-liang Kao <clkao@clkao.org>
2011-10-05 00:51:04 +0800

    make sure that we have coffee

--- utils/build.pl
+++ utils/build.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
 use strict;
 use File::Path;
+use IPC::Cmd qw(can_run);
 my @files = <coffee/*.coffee>;
+
+my $coffee_path = can_run('coffee') or die 'coffee is not installed!';
 
 mkpath ['share/static/gen'];
 for (@files) {
     my ($name) = m/.*?\/([^.]+)\.coffee/;
     print "coffee/$name.coffee -> share/static".$/;
-    system("coffee -c -o share/static/gen coffee/$name.coffee");
+    system("$coffee_path -c -o share/static/gen coffee/$name.coffee");
 }
